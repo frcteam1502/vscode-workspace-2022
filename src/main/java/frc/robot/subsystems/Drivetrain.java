0c39594c53f312ed9b133d50abd95d07204b7e68
Colby Pfeiffer <FRCTeam1502@gmail.com>
2022-01-15 11:27:53 -0500

    Wrote some untested probably not working/correct mecanum drive code :)

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.DriveByJoysticks;
+
+public class Drivetrain extends SubsystemBase {
+  private CANSparkMax frontLeft, frontRight, backLeft, backRight;
+
+  /** Creates a new Drivetrain. */
+  public Drivetrain(CANSparkMax frontleft, CANSparkMax frontRight, CANSparkMax backLeft, CANSparkMax backRight ) {
+    setDefaultCommand(new DriveByJoysticks(this));
+    this.frontLeft = frontleft;
+    this.frontRight = frontRight;
+    this.backLeft = backLeft;
+    this.backRight = backRight;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void move(double ySpeed, double xSpeed, double zRotation) {
+    ySpeed = MathUtil.clamp(ySpeed, -1.0, 1.0);
+    xSpeed = MathUtil.clamp(xSpeed, -1.0, 1.0);
+
+    frontLeft.set(xSpeed + ySpeed + zRotation);
+    frontRight.set(xSpeed - ySpeed - zRotation);
+    backLeft.set(xSpeed - ySpeed + zRotation);
+    backRight.set(xSpeed + ySpeed - zRotation);
+  }
+}
