d9e8b4a2554d63ef495dbea5a89c079c5bd31c38
nickSpruce <emeraldspruce2005@gmail.com>
2022-02-08 19:22:39 -0500

    Jeffery told me to

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,20 +6,23 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import frc.robot.commands.RunIntake;
 import frc.robot.subsystems.Intake;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.PS4Controller.Button;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.Motors;
 import frc.robot.PathFindingConstants.OIConstants;
-import frc.robot.commands.AutoIntake;
 import frc.robot.commands.AutoSimple;
 import frc.robot.commands.DriveByJoysticks;
 import frc.robot.commands.AutoCommand;
 import frc.robot.subsystems.AutoDriveSubsystem;
 import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 
 /**
@@ -43,14 +46,12 @@ public class RobotContainer {
   //Autonomous Commands
   public SendableChooser<Command> m_chooser = new SendableChooser<>();
   
-
-  public AutoIntake autoIntake = new AutoIntake(intake);
   public AutoSimple simpleAuto = new AutoSimple(m_drive, intake);
 
     //Blue 1
-  public ParallelCommandGroup blue1_seg1 = new ParallelCommandGroup(new AutoCommand(m_robotDrive, "Blue1_Seg1"), autoIntake);
+  public ParallelCommandGroup blue1_seg1 = new ParallelCommandGroup(new AutoCommand(m_robotDrive, "Blue1_Seg1"), new StartEndCommand(intake::runIntakeForward, intake::stopIntake, intake));
   public AutoCommand blue1_seg2 = new AutoCommand(m_robotDrive, "Blue1_Seg2");
-  public ParallelCommandGroup blue1_seg3 = new ParallelCommandGroup(new AutoCommand(m_robotDrive, "Blue1_Seg3"), autoIntake);
+  public ParallelCommandGroup blue1_seg3 = new ParallelCommandGroup(new AutoCommand(m_robotDrive, "Blue1_Seg3"), new StartEndCommand(intake::runIntakeForward, intake::stopIntake, intake));
   public AutoCommand blue1_seg4 = new AutoCommand(m_robotDrive, "Blue1_Seg4");
   public AutoCommand blue1_seg5 = new AutoCommand(m_robotDrive, "Blue1_Seg5");
   // TODO: Need to add the "shooting" aspect
@@ -62,6 +63,12 @@ public class RobotContainer {
   public RobotContainer() {
     configureButtonBindings();
     setUpMChooser();
+    m_robotDrive.setDefaultCommand(
+        new RunCommand(
+            () ->
+                m_robotDrive.arcadeDrive(
+                    -m_driverController.getLeftY(), m_driverController.getRightX()),
+            m_robotDrive));
   }
 
   //Sets up the sendable chooser for Autonomous
@@ -77,5 +84,8 @@ public class RobotContainer {
   }
 
   private void configureButtonBindings() {
+    new JoystickButton(m_driverController, Button.kR1.value)
+        .whenPressed(() -> m_robotDrive.setMaxOutput(0.5))
+        .whenReleased(() -> m_robotDrive.setMaxOutput(1));
   }
 }
