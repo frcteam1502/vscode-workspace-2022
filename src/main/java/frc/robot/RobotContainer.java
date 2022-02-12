fa69e499a54343d23a2e01f7a2098502cf2f847e
4dunnj <58223112+4dunnj@users.noreply.github.com>
2022-02-12 14:50:23 -0500

    A load of JSON file changes <3

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,21 +1,15 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import frc.robot.commands.RunIntake;
+import frc.robot.commands.blue1;
 import frc.robot.subsystems.Intake;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.PS4Controller.Button;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.Joysticks;
-import frc.robot.PathFindingConstants.OIConstants;
 import frc.robot.commands.AutoSimple;
 import frc.robot.subsystems.Drivetrain;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class RobotContainer {
   //TeleOp Subsystems
@@ -29,15 +23,9 @@ public class RobotContainer {
   public SendableChooser<Command> m_chooser = new SendableChooser<>();
   
   public AutoSimple simpleAuto = new AutoSimple(m_drive, intake);
-
-    //Blue 1
-  public ParallelCommandGroup blue1_seg1 = new ParallelCommandGroup(m_drive.createTrajectoryCommand("Blue1_Seg1"), new StartEndCommand(intake::runIntakeForward, intake::stopIntake, intake));
-  public Command blue1_seg2 = m_drive.createTrajectoryCommand("Blue1_Seg2");
-  public ParallelCommandGroup blue1_seg3 = new ParallelCommandGroup(m_drive.createTrajectoryCommand("Blue1_Seg3"), new StartEndCommand(intake::runIntakeForward, intake::stopIntake, intake));
-  public Command blue1_seg4 = m_drive.createTrajectoryCommand("Blue1_Seg4");
-  public Command blue1_seg5 = m_drive.createTrajectoryCommand("Blue1_Seg5");
+ 
   // TODO: Need to add the "shooting" aspect
-  public SequentialCommandGroup blue1 = new SequentialCommandGroup(blue1_seg1, blue1_seg2, blue1_seg3, blue1_seg4, blue1_seg5);
+  public blue1 blue1 = new blue1(m_drive, intake);
 
   public RobotContainer() {
     m_drive.setDefaultCommand(new RunCommand(() -> m_drive.move(
