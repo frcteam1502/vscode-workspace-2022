26c84076ed228179a1d8a6824a7cc198fd2aa240
Johnbob7 <69798195+Johnbob7@users.noreply.github.com>
2022-03-02 19:37:02 -0500

    I dont care to write a commit now - Colby

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,47 +25,47 @@ public final class Constants {
 
     public static final Limelight limelight = new Limelight();
 
-    // public enum Encoders {
-    //     LeftArmAngleEncoder (Motors.LEFT_ARM_ANGLE.getEncoder()),
-    //     RightArmAngleEncoder (Motors.RIGHT_ARM_ANGLE.getEncoder()),
-    //     LeftExtenderEncoder (Motors.LEFT_ARM_EXTENDER.getEncoder()),
-    //     RightExtenderEncoder (Motors.RIGHT_ARM_EXTENDER.getEncoder()),
-    //     LeftBabyEncoder (Motors.LEFT_BABY.getEncoder()),
-    //     RightBabyEncoder (Motors.RIGHT_BABY.getEncoder()),
-    //     FlapEncoder (Motors.FLAP.getEncoder());
+    public enum Encoders {
+        LeftArmAngleEncoder (Motors.LEFT_ARM_ANGLE.getEncoder()),
+        RightArmAngleEncoder (Motors.RIGHT_ARM_ANGLE.getEncoder()),
+        LeftExtenderEncoder (Motors.LEFT_ARM_EXTENDER.getEncoder()),
+        RightExtenderEncoder (Motors.RIGHT_ARM_EXTENDER.getEncoder()),
+        LeftBabyEncoder (Motors.LEFT_BABY.getEncoder()),
+        RightBabyEncoder (Motors.RIGHT_BABY.getEncoder()),
+        FlapEncoder (Motors.FLAP.getEncoder());
     
-    //     public RelativeEncoder Encoder;
-    //     Encoders(RelativeEncoder encoder) {
-    //       this.Encoder = encoder;
-    //     }
+        public RelativeEncoder Encoder;
+        Encoders(RelativeEncoder encoder) {
+          this.Encoder = encoder;
+        }
         
-    // }
+    }
 
      public final static class Motors {
         
-    //     // Drivetrain
-    //     public static final CANSparkMax DRIVE_FRONT_LEFT = new CANSparkMax(12, MotorType.kBrushless);
-    //     public static final CANSparkMax DRIVE_FRONT_RIGHT = new CANSparkMax(11, MotorType.kBrushless);
-    //     public static final CANSparkMax DRIVE_BACK_LEFT = new CANSparkMax(17, MotorType.kBrushless);
-    //     public static final CANSparkMax DRIVE_BACK_RIGHT = new CANSparkMax(15, MotorType.kBrushless);
+        // Drivetrain
+        public static final CANSparkMax DRIVE_FRONT_LEFT = new CANSparkMax(12, MotorType.kBrushless);
+        public static final CANSparkMax DRIVE_FRONT_RIGHT = new CANSparkMax(11, MotorType.kBrushless);
+        public static final CANSparkMax DRIVE_BACK_LEFT = new CANSparkMax(17, MotorType.kBrushless);
+        public static final CANSparkMax DRIVE_BACK_RIGHT = new CANSparkMax(15, MotorType.kBrushless);
         
-    //     // Climber
-    //     public static final CANSparkMax LEFT_ARM_ANGLE = new CANSparkMax(4, MotorType.kBrushless);
-    //     public static final CANSparkMax RIGHT_ARM_ANGLE = new CANSparkMax(3, MotorType.kBrushless);
-    //     public static final CANSparkMax LEFT_ARM_EXTENDER = new CANSparkMax(10, MotorType.kBrushless);
-    //     public static final CANSparkMax RIGHT_ARM_EXTENDER = new CANSparkMax(9, MotorType.kBrushless);
-    //     public static final CANSparkMax LEFT_BABY = new CANSparkMax(8, MotorType.kBrushless);
-    //     public static final CANSparkMax RIGHT_BABY = new CANSparkMax(7, MotorType.kBrushless);
+        // Climber
+        public static final CANSparkMax LEFT_ARM_ANGLE = new CANSparkMax(4, MotorType.kBrushless);
+        public static final CANSparkMax RIGHT_ARM_ANGLE = new CANSparkMax(3, MotorType.kBrushless);
+        public static final CANSparkMax LEFT_ARM_EXTENDER = new CANSparkMax(10, MotorType.kBrushless);
+        public static final CANSparkMax RIGHT_ARM_EXTENDER = new CANSparkMax(9, MotorType.kBrushless);
+        public static final CANSparkMax LEFT_BABY = new CANSparkMax(8, MotorType.kBrushless);
+        public static final CANSparkMax RIGHT_BABY = new CANSparkMax(7, MotorType.kBrushless);
 
-    //     // Shooter
-    //     public static final CANSparkMax SHOOTER_RIGHT = new CANSparkMax(13, MotorType.kBrushless);
-    //     public static final CANSparkMax SHOOTER_LEFT = new CANSparkMax(14, MotorType.kBrushless);
+        // Shooter
+        public static final CANSparkMax SHOOTER_RIGHT = new CANSparkMax(13, MotorType.kBrushless);
+        public static final CANSparkMax SHOOTER_LEFT = new CANSparkMax(14, MotorType.kBrushless);
         
         public static final CANSparkMax TURRET = new CANSparkMax(16, MotorType.kBrushless);// TODO: change back from 16 to 5
         
-        // public static final CANSparkMax FLAP = new CANSparkMax(2, MotorType.kBrushless);
+        public static final CANSparkMax FLAP = new CANSparkMax(2, MotorType.kBrushless);
 
-        // public static final CANSparkMax INDEX = new CANSparkMax(6, MotorType.kBrushless);
+        public static final CANSparkMax INDEX = new CANSparkMax(6, MotorType.kBrushless);
         
     }
 
@@ -97,12 +97,12 @@ public final class Constants {
     //     public static final JoystickButton RIGHT_STICK = new JoystickButton(Joysticks.CONTROLLER, 15);
     // }
 
-    // public static final class EncoderMaxes {
-    //     public static final double LEFT_MAX = 0;
-    //     public static final double RIGHT_MAX = 0;
-    //     public static final double LEFT_ANGLE_MAX = 0;
-    //     public static final double RIGHT_ANGLE_MAX = 0;
-    //     public static final double LEFT_BABY_MAX = 0;
-    //     public static final double RIGHT_BABY_MAX = 0;
-    // }
+    public static final class EncoderMaxes {
+        public static final double LEFT_MAX = 0;
+        public static final double RIGHT_MAX = 0;
+        public static final double LEFT_ANGLE_MAX = 0;
+        public static final double RIGHT_ANGLE_MAX = 0;
+        public static final double LEFT_BABY_MAX = 0;
+        public static final double RIGHT_BABY_MAX = 0;
+    }
 }
--- src/main/java/frc/robot/subsystems/AngleFlap.java
+++ src/main/java/frc/robot/subsystems/AngleFlap.java
@@ -2,80 +2,88 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Limelight;
 
-// import frc.robot.Limelight;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import frc.robot.commands.UpdateEncoders;
 
-// import com.revrobotics.CANSparkMax;
-// import com.revrobotics.RelativeEncoder;
-// import frc.robot.commands.UpdateEncoders;
 
-// public class AngleFlap extends SubsystemBase {
-//   /** Creates a new AngleFlap. */
+public class AngleFlap extends SubsystemBase {
+  /** Creates a new AngleFlap. */
+  private static Limelight m_limelight = Constants.limelight;
+  private CANSparkMax flap;
 
-//   private CANSparkMax flap;
-
-//   public AngleFlap(CANSparkMax flap) {
-//     //setDefaultCommand(new Shoot(this));
-//     this.flap = flap;
-
-//   }
+  public AngleFlap(CANSparkMax flap) {
+    //setDefaultCommand(new Shoot(this));
+    this.flap = flap;
+  }
   
 
-//   @Override
-//   public void periodic() {
-//     // This method will be called once per scheduler run
-//   }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
 
-//   //TODO: test shoots at diffrent limelight y levels
-//   private double angle = -1;
-//   private double flapmax = 50;
-//   private double bound1 = -5;
-//   private double bound2 = 5;
-//   private double bound3 = 15;
+  //TODO: test shoots at diffrent limelight y levels
+
+  private double flapmax = 50;
+
+
+  ArrayList<ArrayList<Double>> test = new ArrayList<ArrayList<Double>>();
+
+  ArrayList<Double> arrayA = new ArrayList<Double>();
+  ArrayList<Double> arrayB = new ArrayList<Double>();
+  ArrayList<Double> arrayC = new ArrayList<Double>();
+
   
-//   private double Angle(){
-//     if (!Limelight.target){// no target
-//       angle = -1;
-//     }
-//     else if(Limelight.y <= bound1){//shoot zone 1
-//       angle = Limelight.y - bound1;
-//     }
-//     else if(Limelight.y <= bound2 && Limelight.y > bound1){//shoot zone 2
 
-//     }
-//     else if(Limelight.y <= bound3 && Limelight.y > bound2){//shoot zone 3
-      
-//     }
-//     return angle;
-//   }
+  // public double Shot(double hood, double spled){
+  //   if (!m_limelight.target){// no target
+  //     return ;
+  //   }
+  //   return spled;
+    
 
-//   public void Moveflap(){
+    
+  // }
 
-//     if (Angle() < 0){
-//       flap.set(0);
-//     }
-//     else if ((Angle() >= (EncoderValues.flap + 5)) && (Angle() <=  (EncoderValues.flap - 5))){
-//       flap.set(0);
-//     }
-//     else if (Angle() >= (EncoderValues.flap + 5)){
-//       flap.set(0.05);
-//     }
-//     else if (Angle() <=  (EncoderValues.flap - 5)){
-//       flap.set(-0.05);
-//     }
+  public void Moveflap(){
 
-//     }
+    if (Angle() < 0){
+      flap.set(0);
+    }
+    else if ((Angle() >= (EncoderValues.flap + 5)) && (Angle() <=  (EncoderValues.flap - 5))){
+      flap.set(0);
+    }
+    else if (Angle() >= (EncoderValues.flap + 5)){
+      flap.set(0.05);
+    }
+    else if (Angle() <=  (EncoderValues.flap - 5)){
+      flap.set(-0.05);
+    }
+
+    }
    
 
 
 
-//   public void fResetEncoders() {
-//     this.flap.getEncoder().setPosition(0);
-//   }
+  private int Angle() {
+    return 0;
+  }
+
+
+  public void fResetEncoders() {
+    this.flap.getEncoder().setPosition(0);
+  }
 
 
 
-// }
+}
