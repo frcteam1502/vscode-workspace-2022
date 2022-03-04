876de4fe68e15db4b128dc943f0cd069e16dfa80
Team1502DS <frcteam1502@gmail.com>
2022-03-03 19:00:15 -0600

    Changes

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -41,7 +41,7 @@ public class Shoot extends CommandBase {
       shooter.shoot();
     } else shooter.noShoot();
 
-    if (Joysticks.CONTROLLER.getLeftY() > 0.9) shooter.indexBall();
+    if (Joysticks.CONTROLLER.getRightY() < -0.9) shooter.indexBall();
     else shooter.indexBallStop();
 
     /*
