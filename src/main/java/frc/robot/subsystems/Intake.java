69845b8d20e1be93cfa09bfe50c767bf0a0234ec
Team1502DS <frcteam1502@gmail.com>
2022-04-02 14:25:19 -0500

    it was breaking

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -23,7 +23,7 @@ public class Intake extends SubsystemBase {
     // This method will be called once per scheduler run
   }
   public void moveIntake(){ 
-    intake.set(0.9);
+    intake.set(0.75);
   }
   public void stopIntake(){
     intake.set(0);
